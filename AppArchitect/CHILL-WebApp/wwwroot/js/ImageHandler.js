0ee4dafb946fa22f588817ca30b4f7afbe981196
DevanStrife <113341203+DevanStrife@users.noreply.github.com>
2023-10-30 12:49:36 +0100

    Coordinate system complete

--- AppArchitect/CHILL-WebApp/wwwroot/js/ImageHandler.js
+++ AppArchitect/CHILL-WebApp/wwwroot/js/ImageHandler.js
@@ -1,16 +1,70 @@
-﻿var c1 = document.getElementById("c1"), c2 = document.getElementById("c2");
-var ctx1 = c1.getContext("2d"), ctx2 = c2.getContext("2d");
-ctx2.setLineDash([5, 5]);
-var origin = null;
-window.onload = () => { ctx1.drawImage(document.getElementById("img"), 0, 0); }
-c2.onmousedown = e => { origin = { x: e.offsetX, y: e.offsetY }; };
-window.onmouseup = e => { origin = null; };
-c2.onmousemove = e => {
-    if (!!origin) {
-        ctx2.strokeStyle = "#ff0000";
-        ctx2.clearRect(0, 0, c2.width, c2.height);
-        ctx2.beginPath();
-        ctx2.rect(origin.x, origin.y, e.offsetX - origin.x, e.offsetY - origin.y);
-        ctx2.stroke();
-    }
-};
+﻿const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+const coordinatesDiv = document.getElementById("coordinates");
+const image = document.getElementById("img");
+let isDrawing = false;
+let startX, startY, endX, endY;
+
+image.addEventListener("change", () => {
+    const image = new Image();
+    const file = image.files[0];
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+        image.src = e.target.result;
+        image.onload = () => {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+        };
+    };
+
+    reader.readAsDataURL(file);
+});
+
+canvas.addEventListener("mousedown", (e) => {
+    isDrawing = true;
+    startX = e.clientX - canvas.getBoundingClientRect().left;
+    startY = e.clientY - canvas.getBoundingClientRect().top;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+    if (!isDrawing) return;
+    endX = e.clientX - canvas.getBoundingClientRect().left;
+    endY = e.clientY - canvas.getBoundingClientRect().top;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+    drawRectangle();
+});
+
+canvas.addEventListener("mouseup", () => {
+    isDrawing = false;
+    getRectangleCoordinates();
+});
+
+function drawRectangle() {
+    ctx.beginPath();
+    ctx.rect(startX, startY, endX - startX, endY - startY);
+    ctx.strokeStyle = "blue";
+    ctx.lineWidth = 2;
+    ctx.stroke();
+}
+
+function getRectangleCoordinates() {
+    const topLeft = { x: startX, y: startY };
+    const topRight = { x: endX, y: startY };
+    const bottomLeft = { x: startX, y: endY };
+    const bottomRight = { x: endX, y: endY };
+
+    const roundedTopLeft = { x: topLeft.x.toFixed(2), y: topLeft.y.toFixed(2) };
+    const roundedTopRight = { x: topRight.x.toFixed(2), y: topRight.y.toFixed(2) };
+    const roundedBottomLeft = { x: bottomLeft.x.toFixed(2), y: bottomLeft.y.toFixed(2) };
+    const roundedBottomRight = { x: bottomRight.x.toFixed(2), y: bottomRight.y.toFixed(2) };
+
+    coordinatesDiv.innerHTML = `
+                Top Left: (${roundedTopLeft.x}, ${roundedTopLeft.y})<br>
+                Top Right: (${roundedTopRight.x}, ${roundedTopRight.y})<br>
+                Bottom Left: (${roundedBottomLeft.x}, ${roundedBottomLeft.y})<br>
+                Bottom Right: (${roundedBottomRight.x}, ${roundedBottomRight.y})
+            `;
+}
